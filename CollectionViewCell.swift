c77f1e87186fd89a5b3ed89e76daa4b63d365e52
Truong Nguyen <nguyentruong.xyz0405@gmail.com>
2020-09-11 22:28:40 +0700

    add collection view

---
+++ CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  SimpleCollectionView
+//
+//  Created by Truong Nguyen on 9/11/20.
+//  Copyright © 2020 Truong Nguyen. All rights reserved.
+//
+
+import UIKit
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myLabel: UILabel!
+}
